ee22a32d1106f354abb90e111fccb9212ef0e7c6
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2025-01-17 15:27:11 +0100

    feature: header fix and update from about me and hero for jsx and css

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,8 +17,8 @@ export function Header() {
           <li><FontAwesomeIcon icon={faUser} className="iconNav"/><a href="#Présentation"> Présentation</a></li>
           <li><FontAwesomeIcon icon={faBook} className="iconNav"/><a href="#Projets"> Projets</a></li>
           <li><FontAwesomeIcon icon={faAddressBook} className="iconNav" /><a href="#Contact"> Contact</a></li>
-          <li><FontAwesomeIcon icon={faLinkedin} className="iconNav"/> <a href="https://www.aedin.com/in/florian-lhomel-8995b1285/"> Linkedin</a></li>
-          <li><FontAwesomeIcon icon={faGithub} className="iconNav"/><a href="https://github.com/Luzdeveloper"> Github</a></li>
+          <li><FontAwesomeIcon icon={faLinkedin} className="iconNav"/> <a href="https://www.linkedin.com/in/florian-lhomel-8995b1285/"target="blank"> Linkedin</a></li>
+          <li><FontAwesomeIcon icon={faGithub} className="iconNav"/><a href="https://github.com/Luzdeveloper"target="blank"> Github</a></li>
         </ul>
       </nav>
     </header>
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Header } from './Header'
 import { Hero } from './Hero'
+import { Aboutme } from './Aboutme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <Hero />
+    <Aboutme />
   </StrictMode>,
 )
