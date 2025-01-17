63cc7b84a98edf3401108db423222635cea35de5
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2025-01-17 15:02:44 +0100

    update css style header + hero create

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,7 @@
 import logo from "./assets/logo.webp"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faHouse, faUser, faBook, faAddressBook } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import './styles/header.css'
 
 
@@ -8,15 +9,16 @@ export function Header() {
 
   return(
     <header>
-      <img src={logo} alt="logo"></img>
+      
       <nav>
-        <ul>
-          <li><a href="#Accueil">Accueil</a></li>
-          <li><a href="#Présentation">Présentation</a></li>
-          <li><a href="#Projets">Projets</a></li>
-          <li><a href="#Contact">Contact</a></li>
-          <li><FontAwesomeIcon icon={faLinkedin} /> <a href="https://www.aedin.com/in/florian-lhomel-8995b1285/">Linkedin</a></li>
-          <li><a href="https://github.com/Luzdeveloper">Github</a></li>
+        <img className="logo" src={logo} alt="logo"></img>
+        <ul className="nav-links">
+          <li><FontAwesomeIcon icon={faHouse} className="iconNav" /><a href="#Accueil"> Accueil</a></li>
+          <li><FontAwesomeIcon icon={faUser} className="iconNav"/><a href="#Présentation"> Présentation</a></li>
+          <li><FontAwesomeIcon icon={faBook} className="iconNav"/><a href="#Projets"> Projets</a></li>
+          <li><FontAwesomeIcon icon={faAddressBook} className="iconNav" /><a href="#Contact"> Contact</a></li>
+          <li><FontAwesomeIcon icon={faLinkedin} className="iconNav"/> <a href="https://www.aedin.com/in/florian-lhomel-8995b1285/"> Linkedin</a></li>
+          <li><FontAwesomeIcon icon={faGithub} className="iconNav"/><a href="https://github.com/Luzdeveloper"> Github</a></li>
         </ul>
       </nav>
     </header>
---
+++ src/Hero.jsx
@@ -0,0 +1,10 @@
+import './styles/hero.css';
+
+export function Hero() {
+  return (
+    <section id="Accueil" className="hero">
+      <h1>Bienvenue sur mon portfolio</h1>
+      <p>Développeur FrontEnd</p>
+    </section>
+  );
+}
