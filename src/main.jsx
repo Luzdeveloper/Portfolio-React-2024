2df8a4266d1c06da82d44e0c5fd08787598d918d
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2025-02-12 12:13:36 +0100

    fix main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
-import { AboutMe } from './components/AboutMe';
+import { AboutMe } from './components/Aboutme';
 import { Skills } from './components/Skills';
 import { Project } from './components/Project';
 import { Footer } from './components/Footer';
